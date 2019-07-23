4a1298f7f0a55af66caed90caeb5d4cac291b6d3
Anthony Cooper <53018646+anthony-cooper@users.noreply.github.com>
2019-07-23 18:42:05 +0100

    Test if impact raster has already been loaded into workspace

--- impact_raster_creator.py
+++ impact_raster_creator.py
@@ -17,8 +17,8 @@
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.								   *
- *																		 *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
  ***************************************************************************/
 """
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
@@ -38,278 +38,283 @@ import os.path
 
 class ImpactRasterCreator:
 
-	"""QGIS Plugin Implementation."""
+    """QGIS Plugin Implementation."""
 
-	def __init__(self, iface):
-		"""Constructor.
+    def __init__(self, iface):
+        """Constructor.
 
-		:param iface: An interface instance that will be passed to this class
-			which provides the hook by which you can manipulate the QGIS
-			application at run time.
-		:type iface: QgsInterface
-		"""
-		# Save reference to the QGIS interface
-		self.iface = iface
-		# initialize plugin directory
-		self.plugin_dir = os.path.dirname(__file__)
-		# initialize locale
-		locale = QSettings().value('locale/userLocale')[0:2]
-		locale_path = os.path.join(
-			self.plugin_dir,
-			'i18n',
-			'ImpactRasterCreator_{}.qm'.format(locale))
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
+        """
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        # initialize locale
+        locale = QSettings().value('locale/userLocale')[0:2]
+        locale_path = os.path.join(
+            self.plugin_dir,
+            'i18n',
+            'ImpactRasterCreator_{}.qm'.format(locale))
 
-		if os.path.exists(locale_path):
-			self.translator = QTranslator()
-			self.translator.load(locale_path)
-			QCoreApplication.installTranslator(self.translator)
+        if os.path.exists(locale_path):
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+            QCoreApplication.installTranslator(self.translator)
 
-		# Declare instance attributes
-		self.actions = []
-		self.menu = self.tr(u'&Impact Raster Creator')
+        # Declare instance attributes
+        self.actions = []
+        self.menu = self.tr(u'&Impact Raster Creator')
 
-		# Check if plugin was started the first time in current QGIS session
-		# Must be set in initGui() to survive plugin reloads
-		self.first_start = None
+        # Check if plugin was started the first time in current QGIS session
+        # Must be set in initGui() to survive plugin reloads
+        self.first_start = None
 
-	# noinspection PyMethodMayBeStatic
-	def tr(self, message):
-		"""Get the translation for a string using Qt translation API.
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
 
-		We implement this ourselves since we do not inherit QObject.
+        We implement this ourselves since we do not inherit QObject.
 
-		:param message: String for translation.
-		:type message: str, QString
+        :param message: String for translation.
+        :type message: str, QString
 
-		:returns: Translated version of message.
-		:rtype: QString
-		"""
-		# noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-		return QCoreApplication.translate('ImpactRasterCreator', message)
-
-
-	def add_action(
-		self,
-		icon_path,
-		text,
-		callback,
-		enabled_flag=True,
-		add_to_menu=True,
-		add_to_toolbar=True,
-		status_tip=None,
-		whats_this=None,
-		parent=None):
-		"""Add a toolbar icon to the toolbar.
-
-		:param icon_path: Path to the icon for this action. Can be a resource
-			path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-		:type icon_path: str
-
-		:param text: Text that should be shown in menu items for this action.
-		:type text: str
-
-		:param callback: Function to be called when the action is triggered.
-		:type callback: function
-
-		:param enabled_flag: A flag indicating if the action should be enabled
-			by default. Defaults to True.
-		:type enabled_flag: bool
-
-		:param add_to_menu: Flag indicating whether the action should also
-			be added to the menu. Defaults to True.
-		:type add_to_menu: bool
-
-		:param add_to_toolbar: Flag indicating whether the action should also
-			be added to the toolbar. Defaults to True.
-		:type add_to_toolbar: bool
-
-		:param status_tip: Optional text to show in a popup when mouse pointer
-			hovers over the action.
-		:type status_tip: str
-
-		:param parent: Parent widget for the new action. Defaults None.
-		:type parent: QWidget
-
-		:param whats_this: Optional text to show in the status bar when the
-			mouse pointer hovers over the action.
-
-		:returns: The action that was created. Note that the action is also
-			added to self.actions list.
-		:rtype: QAction
-		"""
-
-		icon = QIcon(icon_path)
-		action = QAction(icon, text, parent)
-		action.triggered.connect(callback)
-		action.setEnabled(enabled_flag)
-
-		if status_tip is not None:
-			action.setStatusTip(status_tip)
-
-		if whats_this is not None:
-			action.setWhatsThis(whats_this)
-
-		if add_to_toolbar:
-			# Adds plugin icon to Plugins toolbar
-			self.iface.addToolBarIcon(action)
-
-		if add_to_menu:
-			self.iface.addPluginToRasterMenu(
-				self.menu,
-				action)
-
-		self.actions.append(action)
-
-		return action
-
-	def initGui(self):
-		"""Create the menu entries and toolbar icons inside the QGIS GUI."""
-
-		icon_path = ':/plugins/impact_raster_creator/icon.png'
-		self.add_action(
-			icon_path,
-			text=self.tr(u'Impact Raster Creator'),
-			callback=self.run,
-			parent=self.iface.mainWindow())
-
-		# will be set False in run()
-		self.first_start = True
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('ImpactRasterCreator', message)
 
 
-	def unload(self):
-		"""Removes the plugin menu item and icon from QGIS GUI."""
-		for action in self.actions:
-			self.iface.removePluginRasterMenu(
-				self.tr(u'&Impact Raster Creator'),
-				action)
-			self.iface.removeToolBarIcon(action)
+    def add_action(
+        self,
+        icon_path,
+        text,
+        callback,
+        enabled_flag=True,
+        add_to_menu=True,
+        add_to_toolbar=True,
+        status_tip=None,
+        whats_this=None,
+        parent=None):
+        """Add a toolbar icon to the toolbar.
+
+        :param icon_path: Path to the icon for this action. Can be a resource
+            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
+        :type icon_path: str
+
+        :param text: Text that should be shown in menu items for this action.
+        :type text: str
+
+        :param callback: Function to be called when the action is triggered.
+        :type callback: function
+
+        :param enabled_flag: A flag indicating if the action should be enabled
+            by default. Defaults to True.
+        :type enabled_flag: bool
+
+        :param add_to_menu: Flag indicating whether the action should also
+            be added to the menu. Defaults to True.
+        :type add_to_menu: bool
+
+        :param add_to_toolbar: Flag indicating whether the action should also
+            be added to the toolbar. Defaults to True.
+        :type add_to_toolbar: bool
+
+        :param status_tip: Optional text to show in a popup when mouse pointer
+            hovers over the action.
+        :type status_tip: str
+
+        :param parent: Parent widget for the new action. Defaults None.
+        :type parent: QWidget
+
+        :param whats_this: Optional text to show in the status bar when the
+            mouse pointer hovers over the action.
+
+        :returns: The action that was created. Note that the action is also
+            added to self.actions list.
+        :rtype: QAction
+        """
+
+        icon = QIcon(icon_path)
+        action = QAction(icon, text, parent)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled_flag)
+
+        if status_tip is not None:
+            action.setStatusTip(status_tip)
+
+        if whats_this is not None:
+            action.setWhatsThis(whats_this)
+
+        if add_to_toolbar:
+            # Adds plugin icon to Plugins toolbar
+            self.iface.addToolBarIcon(action)
+
+        if add_to_menu:
+            self.iface.addPluginToRasterMenu(
+                self.menu,
+                action)
+
+        self.actions.append(action)
+
+        return action
+
+    def initGui(self):
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+
+        icon_path = ':/plugins/impact_raster_creator/icon.png'
+        self.add_action(
+            icon_path,
+            text=self.tr(u'Impact Raster Creator'),
+            callback=self.run,
+            parent=self.iface.mainWindow())
+
+        # will be set False in run()
+        self.first_start = True
 
 
-	def run(self):
-		"""Run method that performs all the real work"""
+    def unload(self):
+        """Removes the plugin menu item and icon from QGIS GUI."""
+        for action in self.actions:
+            self.iface.removePluginRasterMenu(
+                self.tr(u'&Impact Raster Creator'),
+                action)
+            self.iface.removeToolBarIcon(action)
 
-		# Create the dialog with elements (after translation) and keep reference
-		# Only create GUI ONCE in callback, so that it will only load when the plugin is started
-		if self.first_start == True:
-			self.first_start = False
-			self.dlg = ImpactRasterCreatorDialog()
-			self.dlg.pushButton.clicked.connect(self.select_output_folder)
 
-		
-		# Initialise list of level layers
-		levelLayers = []
-		layers = []		
-		# Fetch the currently loaded layers
-		layers = self.load_all_layers(QgsProject.instance().layerTreeRoot().children(), layers)
-		# Clear the contents of the comboBox from previous runs
-		self.dlg.comboBox.clear()
-		for layer in layers:
-			if layer.layer().type() == 1:
-				if (layer.name()).find('h_Max') != -1:
-					self.dlg.comboBox.addItem(layer.name())
-					levelLayers.append(layer)
-				
-				if ((layer.name()).find('BAS') != -1):
-					self.dlg.comboBox.setCurrentIndex(self.dlg.comboBox.count()-1)
-		
-		events = (self.dlg.lineEdit.text()).split(",")
-		
-		joinedLayers = self.create_file_list(levelLayers, events)
+    def run(self):
+        """Run method that performs all the real work"""
 
-		# show the dialog
-		self.dlg.show()
-		# Run the dialog event loop
-		result = self.dlg.exec_()
-		# See if OK was pressed
-		if result:
-			for joinedLayer in joinedLayers:
-				if joinedLayer[3] is True:
-					joinedLayer[5] = self.dlg.outputFolderDlg.text() + '/'  + joinedLayer[4] + '.tif'
-				
-					parameters = {'INPUT_A' : joinedLayer[0].name(), 'INPUT_B' : joinedLayer[1].name(),
-					'BAND_A' : 1, 'BAND_B' : 1,
-					'FORMULA' : '(A - B)',   
-					'OUTPUT' : joinedLayer[5]}
-				
-					processing.run('gdal:rastercalculator', parameters)
-					
-					self.iface.addRasterLayer(joinedLayer[5],joinedLayer[4])
-						
-	
-	def create_file_list(self, levelLayers, events):
-	
-		self.dlg.rasterList.clear()
-		
-		for event in events:
-			eloc = levelLayers[self.dlg.comboBox.currentIndex()].name().find(event)
-			if eloc != -1:
-				baseLayer = (levelLayers[self.dlg.comboBox.currentIndex()].name()).replace(event,'~event~')
-				break
-		
-		baseLayers = []
-		devLayers = []
-		for levelLayer in levelLayers:
-			for event in events:
-				eloc = levelLayer.name().find(event)
-				if eloc != -1:
-					genLayer = (levelLayer.name()).replace(event,'~event~')
-					if genLayer == baseLayer:
-						baseLayers.append([levelLayer, event])
-					else:
-						devLayers.append([levelLayer, event])
-					break
-		
-		joinedLayers = []
-		for devLayer in devLayers:
-			for baseLayer in baseLayers:
-				if devLayer[1] == baseLayer[1]:
-					joinedLayers.append([devLayer[0], baseLayer[0], devLayer[1], True, '', ''])
-					break
+        # Create the dialog with elements (after translation) and keep reference
+        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
+        if self.first_start == True:
+            self.first_start = False
+            self.dlg = ImpactRasterCreatorDialog()
+            self.dlg.pushButton.clicked.connect(self.select_output_folder)
 
-		for joinedLayer in joinedLayers:
-			strSuf = ''
-			strPre = ''
-			strDev = ''
-			strBas = ''
-			for idx, let in enumerate(joinedLayer[0].name()):
-				if let == joinedLayer[1].name()[idx]:
-					strPre = strPre + let
-				else:
-					strDev = joinedLayer[0].name()[(idx-len(joinedLayer[0].name())):]
-					strBas = joinedLayer[1].name()[(idx-len(joinedLayer[1].name())):]
-					break
-					
-			for idx, let in enumerate(reversed(strDev)):
-				if let == strBas[len(strBas)-idx-1]:
-					strSuf = let + strSuf
-				else:
-					strDev = strDev[:(len(strDev)-len(strSuf))]
-					strBas = strBas[:(len(strBas)-len(strSuf))]
-					break
-			
-			joinedLayer[4] = strPre + '[' + strDev + ']-[' + strBas + ']_' + joinedLayer[2] + '_dh'
-			
-			
-			self.dlg.rasterList.addItem(joinedLayer[4])
-		
-		return joinedLayers
-			
-	  
 
-		
-	
-	def load_all_layers(self, group, layers):
-		for child in group:
-			if isinstance(child, QgsLayerTreeLayer):
-				layers.append(child)			
-			elif isinstance(child, QgsLayerTreeGroup):
-				layers = self.load_all_layers(child.children(), layers)
-		return layers
-		
-			
-			
-	def select_output_folder(self):
-		folder = QFileDialog.getExistingDirectory(self.dlg, "Open Directory", "/home", QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
-		self.dlg.outputFolderDlg.setText(folder)
-	  
-	  
+        # Initialise list of level layers
+        levelLayers = []
+        impactLayers = []
+        layers = []
+        # Fetch the currently loaded layers
+        layers = self.load_all_layers(QgsProject.instance().layerTreeRoot().children(), layers)
+        # Clear the contents of the comboBox from previous runs
+        self.dlg.comboBox.clear()
+        for layer in layers:
+            if layer.layer().type() == 1:
+                if (layer.name()).find('h_Max') != -1:
+                    self.dlg.comboBox.addItem(layer.name())
+                    levelLayers.append(layer)
+                elif (layer.name()).find('_dh') != -1:
+                    impactLayers.append(layer)
+
+                if ((layer.name()).find('BAS') != -1):
+                    self.dlg.comboBox.setCurrentIndex(self.dlg.comboBox.count()-1)
+
+        events = (self.dlg.lineEdit.text()).split(",")
+
+        joinedLayers = self.create_file_list(levelLayers, impactLayers, events)
+
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result:
+            for joinedLayer in joinedLayers:
+                if joinedLayer[3] is True:
+                    joinedLayer[5] = self.dlg.outputFolderDlg.text() + '/'  + joinedLayer[4] + '.tif'
+
+                    parameters = {'INPUT_A' : joinedLayer[0].name(), 'INPUT_B' : joinedLayer[1].name(),
+                    'BAND_A' : 1, 'BAND_B' : 1,
+                    'FORMULA' : '(A - B)',
+                    'OUTPUT' : joinedLayer[5]}
+
+                    processing.run('gdal:rastercalculator', parameters)
+
+                    self.iface.addRasterLayer(joinedLayer[5],joinedLayer[4])
+
+
+    def create_file_list(self, levelLayers, impactLayers, events):
+
+        self.dlg.rasterList.clear()
+
+        for event in events:
+            eloc = levelLayers[self.dlg.comboBox.currentIndex()].name().find(event)
+            if eloc != -1:
+                baseLayer = (levelLayers[self.dlg.comboBox.currentIndex()].name()).replace(event,'~event~')
+                break
+
+        baseLayers = []
+        devLayers = []
+        for levelLayer in levelLayers:
+            for event in events:
+                eloc = levelLayer.name().find(event)
+                if eloc != -1:
+                    genLayer = (levelLayer.name()).replace(event,'~event~')
+                    if genLayer == baseLayer:
+                        baseLayers.append([levelLayer, event])
+                    else:
+                        devLayers.append([levelLayer, event])
+                    break
+
+        joinedLayers = []
+        for devLayer in devLayers:
+            for baseLayer in baseLayers:
+                if devLayer[1] == baseLayer[1]:
+                    joinedLayers.append([devLayer[0], baseLayer[0], devLayer[1], True, '', ''])
+                    break
+
+        for joinedLayer in joinedLayers:
+            strSuf = ''
+            strPre = ''
+            strDev = ''
+            strBas = ''
+            for idx, let in enumerate(joinedLayer[0].name()):
+                if let == joinedLayer[1].name()[idx]:
+                    strPre = strPre + let
+                else:
+                    strDev = joinedLayer[0].name()[(idx-len(joinedLayer[0].name())):]
+                    strBas = joinedLayer[1].name()[(idx-len(joinedLayer[1].name())):]
+                    break
+
+            for idx, let in enumerate(reversed(strDev)):
+                if let == strBas[len(strBas)-idx-1]:
+                    strSuf = let + strSuf
+                else:
+                    strDev = strDev[:(len(strDev)-len(strSuf))]
+                    strBas = strBas[:(len(strBas)-len(strSuf))]
+                    break
+
+            joinedLayer[4] = strPre + '[' + strDev + ']-[' + strBas + ']_' + joinedLayer[2] + '_dh'
+
+            for impactLayer in impactLayers:
+                if joinedLayer[4] == impactLayer.name():
+                    joinedLayer[3] == False
+                    break
+
+            self.dlg.rasterList.addItem(joinedLayer[4])
+
+        return joinedLayers
+
+
+
+
+
+    def load_all_layers(self, group, layers):
+        for child in group:
+            if isinstance(child, QgsLayerTreeLayer):
+                layers.append(child)
+            elif isinstance(child, QgsLayerTreeGroup):
+                layers = self.load_all_layers(child.children(), layers)
+        return layers
+
+
+
+    def select_output_folder(self):
+        folder = QFileDialog.getExistingDirectory(self.dlg, "Open Directory", "/home", QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+        self.dlg.outputFolderDlg.setText(folder)
